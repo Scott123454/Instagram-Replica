bbc4cc20117f8b8ff96a9b9453fcc2e0d7111f06
scott123454 <scott123454@hotmail.co.uk>
2014-07-23 21:30:14 +0100

    more indentation work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,12 @@
 class CommentsController < ApplicationController
-  def new
-  	@post = Post.find(params[:post_id])
-  	@comment = Comment.new
-  end
+	def new
+  		@post = Post.find(params[:post_id])
+  		@comment = Comment.new
+  	end
 
-  def create
-  	@post = Post.find(params[:post_id])
-  	@post.comments.create!(params[:comment].permit(:comment))
+  	def create
+  		@post = Post.find(params[:post_id])
+  		@post.comments.create!(params[:comment].permit(:comment))
 		redirect_to '/posts'
-  end
-
+  	end
 end
