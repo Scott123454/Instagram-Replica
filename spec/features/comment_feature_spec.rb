e97bd1c91c3422727ccbae4e4bdb94e6135854e6
scott123454 <scott123454@hotmail.co.uk>
2014-07-25 18:34:05 +0100

    indentation adjustments

--- spec/features/comment_feature_spec.rb
+++ spec/features/comment_feature_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'writing comments' do 
 	before do 
-			user = User.create(email: 'ollie@ollie.com', password: '12345678', password_confirmation: '12345678')
-			login_as user
-			Post.create(description: 'Comment test')
-		end
+		user = User.create(email: 'ollie@ollie.com', password: '12345678', password_confirmation: '12345678')
+		login_as user
+		Post.create(description: 'Comment test')
+	end
 
 	specify 'a post has no comments when first uploaded' do
 		visit '/posts'
