43d4a579fd8227e7694a9bea0a1048bb3851e125
scott123454 <scott123454@hotmail.co.uk>
2014-08-05 08:23:18 +0100

    tests for order page

--- spec/features/order_feature_spec.rb
+++ spec/features/order_feature_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'orders page' do
 	user = User.create(email: 'ollie@ollie.com', password: '12345678', password_confirmation: '12345678')
 	admin = Admin.create(email: 'ollie@ollie.com', password: '12345678', password_confirmation: '12345678')
-	Post.create(description: 'Admin test')
+	Post.create(description: 'Pretty picture')
 
 
 	context 'logged in as admin' do
@@ -27,6 +27,24 @@ describe 'orders page' do
 	end
 
 	context 'with orders' do
+		before do
+			christmas_day = Date.new(2013, 12, 25)
+			Order.create(id: 1, post: post, user: user, created_at: christmas_day)
+			visit '/orders'		
+		end
+	end
+
+		it 'displays the product' do
+		 	expect(page).to have_link 'Pretty picture'
+		end
+
+		it 'displays the customer email' do
+			expect(page).to have_content 'customer@blah.com'
+		end
+
+		it 'displays an order number' do
+			expect(page).to have content '2512130001'
+		end
 
 	end
 
